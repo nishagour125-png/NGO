f49cae00671bc74f9a4dac0af1564843041aaedb
nishagour125-png <nishagour125@gmail.com>
2026-06-06 20:34:22 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,35 @@
+window.onload = function () {
+
+    let userName = prompt("Please enter your name:");
+
+    if (userName) {
+        alert(
+            "Welcome " + userName +
+            "!\n\nThank you for visiting our NGO website.\nTogether, we can support education, environmental protection, and community welfare."
+        );
+    } else {
+        alert(
+            "Welcome to our NGO Website!\nTogether we can make a positive impact in society."
+        );
+    }
+};
+
+function showMessage() {
+    alert(
+        "Thank you for your interest in volunteering!\nYour support can help us create meaningful change in our community."
+    );
+}
+
+
+
+function showMessage() {
+    alert(
+        "Thank you for your interest in volunteering!\nOur team will contact you soon."
+    );
+}
+function registerVolunteer() {
+    alert("Thank you for registering as a volunteer! We will contact you soon.");
+}
+function showMessage() {
+    alert("Thank you for joining us as a volunteer!");
+}
